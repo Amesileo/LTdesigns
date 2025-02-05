977ab3ec34e50ca3f3a6a8b312d132ab3d2cd2e7
George W J Heath-Collins <33731624+Amesileo@users.noreply.github.com>
2025-02-05 17:46:09 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".typed", {
+    strings: ["A DESIGNER.", "AN ANIMATOR.", "A PHOTOGRAPHER."],
+    typeSpeed: 250,
+    backSpace: 150,
+    loop: true
+});
+var typed1 = new Typed(".typed1", {
+    strings: ["MARKETING.", "BRANDING.", "PRINT DESIGN."],
+    typeSpeed: 250,
+    backSpace: 150,
+    loop: true
+})
